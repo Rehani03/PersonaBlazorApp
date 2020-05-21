451bdf7297210e7c9723c22379d6ab943c872460
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-05-21 10:05:35 -0400

    BLL y DAL agregados.

---
+++ PersonaBlazorApp/BLL/PersonasBLL.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonaBlazorApp.DAL;
+using PersonaBlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonaBlazorApp.BLL
+{
+    public class PersonasBLL
+    {
+        public static bool Guardar(Personas personas)
+        {
+            if (!Existe(personas.personaId))//si no existe insertamos
+
+                return Insertar(personas);
+            else
+                return Modificar(personas);
+
+        }
+
+        private static bool Insertar(Personas personas)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Personas.Add(personas);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+
+        private static bool Modificar(Personas personas)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                //marcar la entidad como modificada para que el contexto sepa como proceder
+                contexto.Entry(personas).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                //buscar la entidad que se desea eliminar
+                var auxPersona = contexto.Personas.Find(id);
+                if (auxPersona != null)
+                {
+                    contexto.Personas.Remove(auxPersona);//remover la entidad
+                    paso = contexto.SaveChanges() > 0;
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Personas Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Personas personas;
+
+            try
+            {
+                personas = contexto.Personas.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return personas;
+
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Personas.Any(p => p.personaId == id);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+
+        }
+    }
+}
